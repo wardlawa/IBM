27d6b77b6367f8996584a84fba41ee89f3673b8b
Alison Wardlaw <alisonwardlaw@esc2040a-imac-a-wardlaw.eeb.utoronto.ca>
2016-02-01 10:32:44 -0500

    Population reproduces with recombination and mutation and parents can survive to the next generation, as discussed in meeting with Aneil on Wednesday January 27th

--- IBM.py
+++ IBM.py
@@ -3,6 +3,7 @@ print "hello world"
 import argparse
 import sys
 import random
+import time
 import numpy
 import math
 from collections import namedtuple
@@ -14,16 +15,20 @@ sys.stdout = testFile
 testFile2 = open('test.err','w')
 sys.stderr = testFile2
 
+seeded = random.seed(time.time())
+
 Individual = namedtuple("Individual", "sex y1 y2 x1 x2")
 
 def main():
     print("#%s" % args)
+    print("random seed = %s" % seeded)
     populations = generatePopulation()
-    popStats(populations)
+#    popStats(populations)
     for i in range(args.generations):
+        print "Generation = ", i
         newGenerations = pickParents(populations)
         populations = newGenerations
-        popStats(populations)
+#        popStats(populations)
 #        #check if we need to split the population
 #        if i == args.split_generation:
 #            populations.append(splitPopulation(populations[0]))
@@ -45,6 +50,7 @@ def parseArgs():
     parser.add_argument("-K", "--carrying_capacity", default=10, help="The carrying capacity of the population.", type=int)
     parser.add_argument("-G", "--generations", default=10, help="The maximum number of generations to run the simulation.", type=int)
     parser.add_argument("-b", "--birth_rate", default=2, help="The intrinsic birth rate of females.", type=int)
+    parser.add_argument("-r", "--recombination_rate", default=0.5, help="The recombination rate in males and females.", type=float)
     parser.add_argument("-c", "--cost_persistence", default=0.5, help="The cost to males of possessing persistence value y.", type=float)
     parser.add_argument("-d", "--cost_harassment", default=0.02, type=float, help="The cost to females of a certain mating rate.")
     parser.add_argument("-a", "--encounter_rate", default=1, type=float, help="The encounter rate between males and females")
@@ -75,8 +81,18 @@ that is at least as long as the number of random numbers the function would need
 '''
 Generates a random population based on the input values
 '''
-numpy.random.seed(123)
+#def testFun(a, b):
+#    '''
+#    >>> testFun(2,3)
+#    6
+#    '''
+#    return a*b
+
 def generatePopulation(rands = None):
+    '''
+    >>>print "testing 123"
+    testing 123
+    '''
     population = []
     sex = numpy.random.binomial(1,0.5,args.population_size)
     #0 = female, 1 = male
@@ -84,14 +100,15 @@ def generatePopulation(rands = None):
     std = 1
     persis1 = truncnorm.rvs(a = (0 - args.male_persistence_trait)/std, b = numpy.inf, loc = args.male_persistence_trait, scale = std, size = args.population_size)
     persis2 = truncnorm.rvs(a = (0 - args.male_persistence_trait)/std, b = numpy.inf, loc = args.male_persistence_trait, scale = std, size = args.population_size)
-#    print persis1
-#    print persis2
+#    print "persis1", persis1
+#    print "persis2", persis2
     resis1 = truncnorm.rvs(a = (0 - args.female_resistance_trait)/std, b = numpy.inf, loc = args.female_resistance_trait, scale = std, size = args.population_size)
     resis2 = truncnorm.rvs(a = (0 - args.female_resistance_trait)/std, b = numpy.inf, loc = args.female_resistance_trait, scale = std, size = args.population_size)
-#    print resis1
-#    print resis2
+#    print "resis1", resis1
+#    print "resis2", resis2
     for i in range(args.population_size):
         population.append(Individual(sex[i], persis1[i], persis2[i], resis1[i], resis2[i]))
+#        print "runningPop", population
 #        #put half of the TEs on each haplotype
 #        num_of_tes_haplotype1 = numpy.random.poisson(args.initial_te_number/2)
 #        num_of_tes_haplotype2 = numpy.random.poisson(args.initial_te_number/2)
@@ -100,83 +117,130 @@ def generatePopulation(rands = None):
 #        haplotype2 = sorted(random.sample(range(0,args.genome_size), num_of_tes_haplotype2))
 #        #calculate the fitness of this individual and put it into the population
 #        population.append(calculateFitness(Individual(0.0, haplotype1, haplotype2)))
-#    print population
+#    print "population", population
     return population
 
 def pickParents(population):     
     females = [ind for ind in population if ind.sex == 0]
     males = [ind for ind in population if ind.sex == 1]
-#    print females
-#    print males
+    print "\t len(females)", len(females)
+#    print "males", males
     newGeneration = []
+    numberOfEncounters = numpy.random.poisson(args.encounter_rate, len(females))
+    fecundity = numpy.random.poisson(args.birth_rate, len(females))
+ #   print "numberOfEncounters", numberOfEncounters
+    print "fecundity", fecundity
     for i in range(0, len(females)):
+        print "\t female ", i
         matedMales = []
         #matedMales keeps track of which males an individual female successfully mated with and needs to be cleared for each female
-#        print "female", i
-        numberOfMates = 0
-        testNum =  numpy.random.choice(len(males),size = 2, replace = False)
+#        print "numberOfMates", numberOfMates
+        if numberOfEncounters[i] == 0:
+            #determine if she survives to the next generation
+            pickDeath = random.uniform(0,1)
+            print ("pickDeath %s" % pickDeath)
+            if (args.death_rate_females) <= pickDeath:
+                print "\t continue"
+                continue
+            else: 
+                newGeneration.append(females[i])
+                print "added to new gen!" 
+        encounteredMales = numpy.random.choice(len(males),size = numberOfEncounters[i], replace = False)
         ### Need to deal with what happens when there are so few males in the population that this does not work anymore
-#        print "testNum", testNum
+#        print "encounteredMales", encounteredMales
         #print "length of testNum", len(testNum)
-        for j in range(0, len(testNum)): 
-            #print "testNum", testNum[j]
-            test = matingProb(females[i], males[testNum[j]])
+        for j in range(0, len(encounteredMales)): 
+            test = matingProb(females[i], males[encounteredMales[j]])
             #print "test", test
             randy = random.uniform(0,1)
             #print "randy", randy
             if randy <= test:
 #                print "mated!"
-                numberOfMates = numberOfMates + 1
+                #matedMales += 1
+                #numberOfMates.append(1)
 #                print males[testNum[j]]                
-                matedMales.append(males[testNum[j]])
-            else:
-                pass
-#            print "num of mates", numberOfMates
-#            print "matedMales", matedMales
+                matedMales.append(males[encounteredMales[j]])
+            print "num of mates", len(matedMales)
+            print "matedMales", matedMales
+        
+        dads = []
+        for i in range(fecundity[i]):
+            dads.append(numpy.random.randint(0, len(matedMales)))
+        print ("dads %s" % (dads))
+        siringSuccess = [dads.count(i) for i in range(len(matedMales))]
+        print (" siringSuccess %s" % (siringSuccess))
+       #siringSuccess prints out a list of how many babies each mated male sires given the fecundity of the female 
+        #probs = [float(num)/len(numberOfMates) for num in numberOfMates]
+        #siringSuccess = numpy.random.multinomial(len(matedMales), probs)
+        #print "siringSuccess", siringSuccess
         for k in range(0, len(matedMales)):
-            makeBabies(females[i], matedMales[k], numberOfMates, newGeneration)
-#    print "newGeneration", newGeneration
+            for babes in range(0, siringSuccess[k]):
+                makeBabies(females[i], matedMales[k], siringSuccess[k], fecundity[i], newGeneration)
+        pickDeath = random.uniform(0,1)
+        print ("pickDeathFemales %s" % pickDeath)
+        print ("double check number of mates %s" % len(matedMales))
+        if (args.death_rate_females + args.cost_harassment*len(matedMales)) <= pickDeath:
+            continue
+        else: 
+            newGeneration.append(females[i])
+    for m in range(0,len(males)):
+        pickDeath=random.uniform(0,1)
+        print("pickDeathMales %s" % pickDeath)
+        if (args.death_rate_males + args.cost_persistence*(males[m].y1 + males[m].y2)/2) <= pickDeath:
+            continue
+        else:
+            newGeneration.append(males[m])
+    print "newGeneration", newGeneration
     return newGeneration
+
 
 def matingProb(female, male):
     resistanceTrait = (female.x1 + female.x2)/2
-    persistenceTrait = (male.y1 + male.y1)/2
+    persistenceTrait = (male.y1 + male.y2)/2
     phi = 1/(1 + math.exp(-(persistenceTrait-resistanceTrait)))
     return phi
 
-def makeBabies(female, male, mates,newPop):
-    for l in range(args.birth_rate/mates):
-        sex = numpy.random.binomial(1,0.5,args.birth_rate/mates)
-#        print "sex", sex
-        pickChromosome1 = random.uniform(0,1)
-        if pickChromosome1 <= 0.5:
-            chromosome1 = [female.x1, female.y1]
+def makeBabies(female, male, siring, fecun, newPop):
+    sex = numpy.random.binomial(1,0.5,1)
+    print ("sex %s" % (sex))
+    chromosome1 = chooseAlleles(female)
+    chromosome2 = chooseAlleles(male)
+    genotype = chromosome1 + chromosome2
+#    print "genotype", genotype
+#    print "length", len(genotype)
+    mut = numpy.random.uniform(low = 0, high = 1, size = 4)
+#    print "mut", mut
+    std = 1
+        ###Maybe could use popStats to give current standard deviation and use that???$######
+    for m in range (0,len(mut)):
+        if mut[m] <= args.mutation_host:
+            tempGenotype = genotype[m] + numpy.random.normal(0, std)
+            genotype[m] = tempGenotype[0]
+                #####Ask Robert if we can just set genotype[m] = truncnorm.rvs etc#######
         else:
-            chromosome1 = [female.x2, female.y2]
-        pickChromosome2 = random.uniform(0,1)
-        if pickChromosome2 < 0.5:
-            chromosome2 = [male.x1, male.y1]
-        else:
-            chromosome2 = [male.x2, male.y2]
-        genotype = chromosome1 + chromosome2
-#        print "genotype", genotype
-#        print "length", len(genotype)
-        mut = numpy.random.uniform(low = 0, high = 1, size = 4)
-#        print "mut", mut
-        for m in range (0,len(mut)):
-            if mut[m] <= args.mutation_host:
-                coinFlip = numpy.random.binomial(1,0.5,1)
-#                print coinFlip
-                if coinFlip == 0:
-                    genotype[m] = genotype[m] + numpy.random.normal(genotype[m],1)
-                else:
-                    genotype[m] = genotype[m] - numpy.random.normal(genotype[m],1) 
-            else:
-                pass
-        offspring = Individual(sex[l],genotype[1], genotype[3], genotype[0], genotype[2])
-#        print "offspring", offspring
-        newPop.append(offspring)        
+            pass
+    offspring = Individual(sex[0], genotype[1], genotype[3], genotype[0], genotype[2])
+    print "offspring", offspring
+    newPop.append(offspring)        
 
+def chooseAlleles(ind):
+    pickX = random.uniform(0,1)
+    if pickX <= 0.5:
+        offspringX1 = ind.x1
+        pickRecomb = random.uniform(0,1)
+        if pickRecomb <= args.recombination_rate:
+            offspringY1 = ind.y2
+        else:
+            offspringY1 = ind.y1
+    else:
+        offspringX1 = ind.x2
+        pickRecomb = random.uniform(0,1)
+        if pickRecomb <= args.recombination_rate:
+            offspringY1 = ind.y1
+        else:
+            offspringY1 = ind.y2
+    return [offspringX1, offspringY1]
+ 
 def popStats(population):
     yVals = []
     xVals = []
@@ -189,50 +253,13 @@ def popStats(population):
     stdY = numpy.std(yVals)
     avgX = numpy.average(xVals)
     stdX = numpy.std(xVals)
-    print avgY, stdY, avgX, stdX
+    females = [ind for ind in population if ind.sex == 0]
+    males = [ind for ind in population if ind.sex == 1]
+####Possible way to save time by using this count from pickParents####
+    print avgY, stdY, avgX, stdX, len(males), len(females)
                 
 
-def pickParent(population, other_parent = None, rands = None):
-    '''
-    >>> args = {'population_size':5, 'max_generations':10, 'cost_persistence':0.5, 'cost_harassment':0.02, 'encounter_rate':1, 'death_rate_males':0.2, 'death_rate_females':0.2, 'birth_rate':2}
-    
-    >>> population = [Individual(0, 1, 5),Individual(1, 2, 5),Individual(1, 2, 3), Individual(0, 2, 3), Individual(0, 1, 4)]
-    
-    #picks the highest fitness
-    >>> pickParent(population, rands = [0.65])
-    Individual(fitness=0.5, haplotype1=[1], haplotype2=[2, 3])
- 
-    #picks the highest fitness
-    >>> parent1 = pickParent(population, rands = [1.0])
-    >>> parent1
-    Individual(fitness=0.5, haplotype1=[1], haplotype2=[2, 3])
-    
-    #would pick the highest again but fails the "other" test
-    >>> pickParent(population, other_parent=parent1, rands = [0.7, 0.27])
-    Individual(fitness=0.4, haplotype1=[], haplotype2=[])
- 
-    #without "other" defined this one should pick the highest fitness again
-    >>> pickParent(population, rands = [0.7])#the rands here should match those in the test above
-    Individual(fitness=0.5, haplotype1=[1], haplotype2=[2, 3])
-
-    #picks the lowest fitness
-    >>> pickParent(population, rands = [0.07])
-    Individual(fitness=0.1, haplotype1=[1, 7, 8], haplotype2=[])
-    
-    #picks the lowest fitness
-    >>> pickParent(population, rands = [0.0])
-    Individual(fitness=0.1, haplotype1=[1, 7, 8], haplotype2=[])
-
-    #picks highest when there is 1 negative fitness
-    >>> population.append(Individual(-0.1, [], []))
-    >>> pickParent(population, rands = [.9])
-    Individual(fitness=0.5, haplotype1=[1], haplotype2=[2, 3])
-    
-    #picks lowest when there is 1 negative fitness
-    >>> pickParent(population, rands = [0])
-    Individual(fitness=-0.1, haplotype1=[], haplotype2=[])
-   ''' 
-    
+def pickParent(population, other_parent = None, rands = None): 
     #this scaling factor helps us deal with negative fitnesses by making all the numbers positive
     scaling_factor = abs(min([individual.fitness for individual in population])) + 0.001
     total_fitness = sum([individual.fitness+scaling_factor for individual in population]) 
@@ -246,11 +273,6 @@ def pickParent(population, other_parent = None, rands = None):
         if cummulative_fitness >= chosen_fitness:
             return individual
     return individual
-
-#def matingProb(male, female):
-#    phi = 1/(1 + math.exp(-(male.y-female.x)))
-#    print phi
-#    return phi
 
 if __name__ == "__main__":
     args = parseArgs()
